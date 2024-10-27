f9d814ae539c996a613da2719fec43922028bbf8
ahmednule <nuleahmed6@gmail.com>
2024-10-27 15:40:48 +0300

    for the urls, the home one will book_list then one for signup, book dor taking, registering, and one for borrowing and returning

---
+++ Library_management_system/library/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('signup/', views.signup, name='signup'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/new/', views.book_create, name='book_create'),
+    path('book/<int:pk>/edit/', views.book_update, name='book_update'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('borrowed/', views.borrowed_books, name='borrowed_books'),
+    path('borrowed/<int:pk>/return/', views.return_book, name='return_book'),
+]
