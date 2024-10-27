36491f15cc3fd0e3d25feeb3e3fbc4a03162d928
ahmednule <nuleahmed6@gmail.com>
2024-10-27 15:28:36 +0300

    update: started with implementing the models,py file added 2 models books and borrow book for when users will be using to borrow books from the application

--- Library_management_system/library/models.py
+++ Library_management_system/library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    publication_date = models.DateField()
+    available_copies = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class BorrowedBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrowed_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField()
+    returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
