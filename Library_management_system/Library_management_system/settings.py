f667641913ab93192481896f5af988159b2dd133
ahmednule <nuleahmed6@gmail.com>
2024-10-27 21:28:46 +0300

    small fix

--- Library_management_system/Library_management_system/settings.py
+++ Library_management_system/Library_management_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#g!-pl6_m+tuzhcpj(p=w*@t445$o*a#x))zn%n+*)ijmh6cp1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://beckam-jossy-library.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['beckam-jossy-library.onrender.com','localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://beckam-jossy-library.onrender.com']
 
 
