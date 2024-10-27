149d90e7ccc44bdb43a0f650d9dd6625e4dba8ec
ahmednule <nuleahmed6@gmail.com>
2024-10-27 16:05:59 +0300

    aded the profile details inside forms.py file

--- Library_management_system/library/forms.py
+++ Library_management_system/library/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Book, BorrowedBook
+from .models import Profile
+from django.contrib.auth.models import User
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -23,3 +25,18 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'address', 'phone_number', 'birth_date', 'profile_picture']
+        widgets = {
+            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+        }
