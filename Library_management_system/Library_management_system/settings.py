4a88296e45c68c50ccdb0012f6945759aa019a6b
ahmednule <nuleahmed6@gmail.com>
2024-10-27 21:50:13 +0300

    update: the url

--- Library_management_system/Library_management_system/settings.py
+++ Library_management_system/Library_management_system/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-#g!-pl6_m+tuzhcpj(p=w*@t445$o*a#x))zn%n+*)ijmh6cp1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beckam-jossy-library.onrender.com','localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://beckam-jossy-library.onrender.com']
+ALLOWED_HOSTS = ['library-z10b.onrender.com','localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://library-z10b.onrender.com']
 
 
 # Application definition
